4ecbcd84b58056ceba7291f1258bb07a62f188e3
Tasmia Khan <145281955+tassmiia@users.noreply.github.com>
2024-07-24 20:14:14 +0530

    Update BentoGrid.tsx

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "khantasmia786@gmail.com";
+    const text = "contact.tasmiia@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
